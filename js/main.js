7bfd4ff110899d826d55e5a8506d581230d1e476
TomasVorac <32869506+TomasVorac@users.noreply.github.com>
2019-02-28 14:16:10 +0100

    Fix the variable notation in main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // create random matrix
 // attr(rows, colums, probability_of_live_cell)
-let random_matrix = createRandomMatrix(10,10,0.5);
+const randomMatrix = createRandomMatrix(10,10,0.5);
 
 // special type of matrix (Oscillators)
-let  oscillators_matrix = [[false, false, true, false], [true, false, false, true], [true, false, false, true], [false, true, false, false]];
+const  oscillatorsMatrix = [[false, false, true, false], [true, false, false, true], [true, false, false, true], [false, true, false, false]];
 
 console.log('%c Oscillators matrix ', 'color: green');
 // start program
 // attr (number_of_generations, zero_matrix)
-main(4, oscillators_matrix);
+main(4, oscillatorsMatrix);
 
 console.log('*********************');
 console.log('%c Random matrix ', 'color: green');
-main(5, random_matrix);
+main(5, randomMatrix);
 
 
 
